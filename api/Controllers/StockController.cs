3a3e42499cdeb8ed460b06feb109061ad4efd363
Keigen Godlaski <godlaska@miamioh.edu>
2025-05-22 15:52:15 -0400

    Added getStockById and getAll methods

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using api.Data;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            // Check if the stock exists
+            if (stock == null) {
+                return NotFound();
+            }
+
+            // Return the stock
+            return Ok(stock);
+        }
+    }
+}
