916e4e498bef2c0ba24fc827a5aa20dfde51ddae
Keigen Godlaski <godlaska@miamioh.edu>
2025-05-28 14:30:06 -0400

    Portfolio GET implementation

---
+++ api/Repository/PortfolioRepository.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository
+{
+    public class PortfolioRepository : IPortfolioRepository
+    {
+        private readonly ApplicationDBContext _context;
+        public PortfolioRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Stock>> GetUserPortfolio(AppUser user)
+        {
+            return await _context.Portfolios
+                .Where(u => u.AppUserId == user.Id)
+                .Select(s => new Stock
+                {
+                    Id = s.StockId,
+                    Symbol = s.Stock.Symbol,
+                    CompanyName = s.Stock.CompanyName,
+                    Purchase = s.Stock.Purchase,
+                    LastDiv = s.Stock.LastDiv,
+                    Industry = s.Stock.Industry,
+                    MarketCap = s.Stock.MarketCap
+                }).ToListAsync();
+        }
+    }
+}
